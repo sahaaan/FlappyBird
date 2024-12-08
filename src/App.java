de9b87c81150e4ebc477f6096d45896ca80eaa27
Sahan Perera <sahaaan123@gmail.com>
2024-12-08 23:36:53 +0530

    Initial commit: Flappy Bird game implementation

---
+++ src/App.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        JFrame frame = new JFrame("Flappy Bird");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(360, 640);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+
+        FlappyBird flappyBird = new FlappyBird();
+        frame.add(flappyBird);
+        frame.pack();
+        flappyBird.requestFocus();
+        frame.setVisible(true);
+    }
+}
